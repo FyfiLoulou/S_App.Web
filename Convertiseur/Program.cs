36ef0350e822ea7646ddf0c2355791845fbe89a5
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-01-26 16:26:16 -0500

    iiiii + fareneithhh

--- Convertiseur/Program.cs
+++ Convertiseur/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Convertiseur
+﻿using System.Text.RegularExpressions;
+
+namespace Convertiseur
 {
     internal class Program
     {
@@ -6,7 +8,62 @@
         {
             Console.WriteLine("Hello, World!");
             Program p = new Program();
+            p.run();
         }
 
+
+        public double saisi()
+        {
+            Console.WriteLine("Veuillez taper un nombre représentant des degrés");
+
+            var a = Console.ReadLine();
+            double d;
+            while (!double.TryParse(a, out d))
+            {
+                Console.WriteLine("YO big ta pas compris dumbass");
+                a = Console.ReadLine();
+            }
+            return d;
+        }
+
+        public double convK(double c)
+        {
+            return c + 273.15;
+        }
+
+        public double convF(double c)
+        {
+            return (c * (9 / 5)) + 32;
+        }
+
+        public bool whichOne()
+        {
+            Console.WriteLine("Tu veux le convertir en kelvin ou en Fahrenheit ?");
+            string ans = Console.ReadLine();
+            ans = ans.ToLower();
+            Regex a = new Regex("[kelvin]");
+            MatchCollection matchCollection = a.Matches(ans);
+            return matchCollection.Count > 4;
+        }
+
+
+        public void run()
+        {
+            double a = saisi();
+            double ans;
+            string fin;
+            if (whichOne())
+            {
+                ans = convK(a);
+                fin = "kelvin";
+            }
+            else
+            {
+                ans = convF(a);
+                fin = "fahreneith";
+            }
+
+            Console.WriteLine($"{ans} degrée {fin} genre dude");
+        }
     }
 }
