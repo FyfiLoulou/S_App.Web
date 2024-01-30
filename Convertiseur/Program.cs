1a3d57253019a6e16a2781dbc1fafe4a367b8338
Alakazouu <2033482@cegeplimoilou.ca>
2024-01-30 12:51:52 -0500

    xseftgy

--- Convertiseur/Program.cs
+++ Convertiseur/Program.cs
@@ -5,7 +5,7 @@
         //Convertisseur (Celcius, Kelvin et Fahrenheit)
 
 
-        // 0C -> 273,15K 
+        // 0C -> 273,15K = 0°C + 273,15
         public String CAK(double c)
         {
             double kelvin = c + 273.15;
@@ -14,21 +14,26 @@
             return message;
         }
 
-        // C -> F = (0°C × 9/5) + 32 = 32°F
+        
         public String CAF(double c)
         {
+            // 0C -> 32F = (0°C × 9/5) + 32
             double fahrenheit = (c * (9 / 5)) + 32;
             String message = c + "°C = " + fahrenheit + "°F";
             
             return message;
         }
+
         static void Main(string[] args)
         {
             Program celcius = new Program();
 
-            Console.WriteLine(celcius.CAK(0.0));
-            Console.WriteLine(celcius.CAF(0.0));
+
+
+            Console.WriteLine(celcius.CAK(1));
+            Console.WriteLine(celcius.CAF(2));
             
+
 
         }
 
---
+++ MiniCalcul/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MiniCalcul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MiniCalcul:");
+
+        }
+    }
+}
