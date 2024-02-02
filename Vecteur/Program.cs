8a020b75050a30439012a2b33a4894d49dadfcd0
Felix Blanchette <1742659@cegeplimoilou.ca>
2024-02-02 16:19:27 -0500

    démerder vous avec la fusion

---
+++ Vecteur/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace Vecteur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ArrayList b = initialisation();
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            trierArray(b);
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            
+        }
+
+        private static ArrayList initialisation()
+        {
+            ArrayList list = new ArrayList();
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(rnd.Next(33, 127));
+            }
+            affichage(list);
+
+            return list;
+        }
+
+        private static void trierArray(ArrayList list)
+        {
+            ArrayList a = new ArrayList();
+            foreach (int i in list)
+            {
+                a.Add(i);
+            }
+            a.Sort();
+            affichage(a);
+        }
+
+
+        private static void affichage(ArrayList list)
+        {
+            foreach (int i in list)
+            {
+                Console.Write($"{i}, "); 
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
