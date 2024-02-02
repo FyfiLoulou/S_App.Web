792127dd192a7f69b4c4f7590d9b834695491466
Alakazouu <2033482@cegeplimoilou.ca>
2024-02-02 15:01:24 -0500

    création du projet produits

---
+++ Produits/Produit.cs
@@ -0,0 +1,35 @@
+﻿namespace Produits
+{
+    internal class Produit
+    {
+        public int noProduit;
+        public String nomProduit;
+        public String description;
+        public double prix;
+        public int quantite;
+        public int qteRupture;
+        public Boolean taxable;
+
+        public Produit(int noProduits, String nomProduits)
+        {
+            noProduit = noProduits;
+            nomProduit = nomProduits;
+            description = "";
+            prix = 0;
+            quantite = 0;
+            qteRupture = 0;
+            taxable = false;
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================Produits=================================");
+
+        }
+    }
+}
